bfda8eaad3d1ba55dd2df3ffb4b4b2bd7be4c3ff
SubSkiol <antpatru2@gmail.com>
2024-01-16 16:20:06 -0500

    Added more user options, removed "Chosen"

--- SACreativeAssistant_4.0.py
+++ SACreativeAssistant_4.0.py
@@ -65,54 +65,54 @@ class CharacterBuilder():
     @staticmethod
     def base_attributes():
         return {
-            "topics":["Chosen Topic: ", ran.choice(character_options.base_attributes()["topics"])],
-            "gender":["Chosen Gender: ", ran.choice(character_options.base_attributes()["gender"])],
-            "build":["Chosen Build: ", ran.choice(character_options.base_attributes()["build"])],
-            "hair":["Chosen Hair: ", ran.choice(character_options.base_attributes()["hair"])],
-            "hair_color":["Chosen Hair Color: ", ran.choice(character_options.base_attributes()["hair_color"])],
+            "topics":["Topic: ", ran.choice(character_options.base_attributes()["topics"])],
+            "gender":["Gender: ", ran.choice(character_options.base_attributes()["gender"])],
+            "build":["Build: ", ran.choice(character_options.base_attributes()["build"])],
+            "hair":["Hair: ", ran.choice(character_options.base_attributes()["hair"])],
+            "hair_color":["Hair Color: ", ran.choice(character_options.base_attributes()["hair_color"])],
         }
     
     @staticmethod
     def human_options():
         return {
-            "occupation":["Chosen Occupation: ", ran.choice(character_options.human_options()["occupation"])],
-            "clothing_style":["Chosen Clothing Stye: ", ran.choice(character_options.human_options()["clothing_style"])],
-            "ethnicity":["Chosen Ethnicity: ", ran.choice(character_options.human_options()["ethnicity"])],
-            "nationality":["Chosen Nationality: ", ran.choice(character_options.human_options()["nationality"])],
-            "age":["Chosen Age: ", ran.choice(character_options.human_options()["age"])],
-            "facial_features":["Chosen Features: ", ran.choice(character_options.human_options()["facial_features"])],
+            "occupation":["Occupation: ", ran.choice(character_options.human_options()["occupation"])],
+            "clothing_style":["Clothing Stye: ", ran.choice(character_options.human_options()["clothing_style"])],
+            "ethnicity":["Ethnicity: ", ran.choice(character_options.human_options()["ethnicity"])],
+            "nationality":["Nationality: ", ran.choice(character_options.human_options()["nationality"])],
+            "age":["Age: ", ran.choice(character_options.human_options()["age"])],
+            "facial_features":["Features: ", ran.choice(character_options.human_options()["facial_features"])],
         }
     
     @staticmethod
     def alien_options():
         return {
-            "type": ["Chosen Alien Type: ", ran.choice(character_options.alien_options()["type"])],
-            "tech_level": ["Chosen Tech Level: ", ran.choice(character_options.alien_options()["tech_level"])],
+            "type": ["Alien Type: ", ran.choice(character_options.alien_options()["type"])],
+            "tech_level": ["Tech Level: ", ran.choice(character_options.alien_options()["tech_level"])],
             #True Alien settigns moved to true_alien_options
         }
     
     @staticmethod
     def true_alien_options():
         return {
-            "eye_number": ["Chosen Eye Number: ", ran.choice(character_options.alien_options()["eye_number"])],
-            "limb_number": ["Chosen Limb Number: ", ran.choice(character_options.alien_options()["limb_number"])],
-            "pattern": ["Chosen Pattern: ", ran.choice(character_options.alien_options()["pattern"])],
-            "antenna": ["Chosen Antenna: ", ran.choice(character_options.alien_options()["antenna"])],
-            "leg_type": ["Chosen Leg Type: ", ran.choice(character_options.alien_options()["leg_type"])],
+            "eye_number": ["Eye Number: ", ran.choice(character_options.alien_options()["eye_number"])],
+            "limb_number": ["Limb Number: ", ran.choice(character_options.alien_options()["limb_number"])],
+            "pattern": ["Pattern: ", ran.choice(character_options.alien_options()["pattern"])],
+            "antenna": ["Antenna: ", ran.choice(character_options.alien_options()["antenna"])],
+            "leg_type": ["Leg Type: ", ran.choice(character_options.alien_options()["leg_type"])],
             }
     @staticmethod
     def animal_options():
         return {
-            "species": ["Chosen Species: ", ran.choice(character_options.animal_options()["species"])],
-            "pattern": ["Chosen Animal Pattern: ", ran.choice(character_options.animal_options()["pattern"])],
-            "size": ["Chosen Size: ", ran.choice(character_options.animal_options()["size"])],
+            "species": ["Species: ", ran.choice(character_options.animal_options()["species"])],
+            "pattern": ["Animal Pattern: ", ran.choice(character_options.animal_options()["pattern"])],
+            "size": ["Size: ", ran.choice(character_options.animal_options()["size"])],
         }
     
     @staticmethod
     def monster_options():
         return {
-            "alignment": ["Chosen Alignment: ", ran.choice(character_options.monster_options()["alignment"])],
-            "type": ["Chosen Monster Type: ", ran.choice(character_options.monster_options()["type"])],
+            "alignment": ["Alignment: ", ran.choice(character_options.monster_options()["alignment"])],
+            "type": ["Monster Type: ", ran.choice(character_options.monster_options()["type"])],
         }
 character_builder = CharacterBuilder()
 
@@ -159,10 +159,11 @@ def monster_handler():
     for key in character_builder.monster_options().keys():
         print_handler(character_builder.monster_options()[key][1], pretext=character_builder.monster_options()[key][0])
 
-def base_handler():
-    print("--CHARACTERISTICS--")
-    chosen_character_base = character_builder.base_attributes()["topics"][1] #handle topics different than the rest as topics has specific tasks relying on what it is. 
-    print_handler(chosen_character_base, pretext=character_builder.base_attributes()["topics"][0])
+def base_handler(bypass: Optional[bool] = False):
+    print("\n--CHARACTERISTICS--")
+    if not bypass:
+        chosen_character_base = character_builder.base_attributes()["topics"][1] #handle topics different than the rest as topics has specific tasks relying on what it is. 
+        print_handler(chosen_character_base, pretext=character_builder.base_attributes()["topics"][0])
     
     for key in ["gender", "build"]: #both of these are handled as normal
         print_handler(character_builder.base_attributes()[key][1], pretext=character_builder.base_attributes()[key][0])
@@ -174,16 +175,16 @@ def base_handler():
     if chosen_hair_length is not None:
         print_handler(character_builder.base_attributes()["hair_color"][1], pretext=character_builder.base_attributes()["hair_color"][0])
         
-
-    #Start the other handlers 
-    if chosen_character_base == "Human":
-        human_handler()
-    if chosen_character_base == "Alien":
-        alien_handler()
-    if chosen_character_base == "Animal":
-        animal_handler()
-    if chosen_character_base == "Monster":
-        monster_handler()    
+    if not bypass:
+        #Start the other handlers 
+        if chosen_character_base == "Human":
+            human_handler()
+        if chosen_character_base == "Alien":
+            alien_handler()
+        if chosen_character_base == "Animal":
+            animal_handler()
+        if chosen_character_base == "Monster":
+            monster_handler()    
 
 
 #Main Program
@@ -192,15 +193,33 @@ def main():
     
     #Various Variables
     version_name = "SubAlternate's Creativity Assistant"
-    version_number = "Version 4.0"
+    version_number = "Version 4.1"
     
     print(f"Runing {version_name} {version_number}")
     while main_loop:
-        main_input = input(f"(R)andomize (Q)uit\n")
+        main_input = input(f"\n(R)andomize (Q)uit\nHuman, Animal, Alien, Monster\n")
         if main_input.lower() == "r":
             base_handler()
+            
         elif main_input.lower() == "q":
             main_loop = False
+            
+        elif main_input.lower() == "human":
+            base_handler(bypass=True)
+            human_handler()
+            
+        elif main_input.lower() == "alien":
+            base_handler(bypass=True)
+            alien_handler()
+            
+        elif main_input.lower() == "animal":
+            base_handler(bypass=True)
+            animal_handler()
+            
+        elif main_input.lower() == "monster":
+            base_handler(bypass=True)
+            monster_handler()
+            
         else:
             print(f"{main_input} is not a valid input!")
 
